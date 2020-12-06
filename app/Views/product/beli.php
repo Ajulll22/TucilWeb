afea9ef3f58c9f78b7e8ff92832fced4f4206035
Ajulll22 <ajulrizki@gmail.com>
2020-12-06 17:51:42 +0700

    tucil

--- app/Views/product/beli.php
+++ app/Views/product/beli.php
@@ -4,32 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Product</title>
+    <title>Beli</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-</head>
-<body>
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h2 class="mt-3">Detail Informasi Produk</h2>
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row no-gutters">
-                    <div class="col-md-4">
-                    <img src="<?php echo base_url('upload/product/'.$product['product_img']) ?>" class="card-img" alt="..." />
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $product['product_name']; ?></h5>
-                            <p class="card-text"><?php echo $product['product_description']; ?></p>
-                            <p class="card-text"><small class="text-muted">Rp. <?php echo $product['product_price']; ?></small></p>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
 
-                            <a href="/dashboard">HOME</a>
+</head>
+
+<section id="abouts" class="abouts">
+
+    <div class="container mt-5 mb-5 text-center">
+        <h4>Detail Product</h4>
+    </div>
+
+    <div class="container">
+        <h4><a href="/dashboard">HOME</a></h4>
+    </div>
+
+                        <div class="container text-center">
+                            <img src="<?php echo base_url('upload/product/'.$product['product_img']) ?>"  width="500" height="500" />
+
+
+                                <h3><?php echo $product['product_name']; ?></h3>
+                                <h4 class="font-weight-bold text-primary">Rp. <?php echo $product['product_price']; ?></h4>
+                                <p><?php echo $product['product_description']; ?></p>
+
+
+
+                                <a href="#portfolio" class="btn btn-primary"> Beli</a>
+                                <h4>.</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        </section>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
--- app/Views/product/create.php
+++ app/Views/product/create.php
@@ -7,12 +7,14 @@
     <title>Product</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/Ajulll22/CSS/main/style.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css') ?>">
 </head>
 <body>
 <section id="abouts" class="abouts">
     <div class="container mt-5 mb-5 text-center">
-
+    </div>
+    <div class="container mt-5 mb-5 text-center">
+        <h4>Post Stock Produk Kopi</h4>
     </div>
     <div class="container">
         <h4>Form Tambah Produk</h4>
